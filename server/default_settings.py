ed7a2d364022726939558b7d63fc58573f1aa878
Lance Hartung <hartung@cs.wisc.edu>
2021-11-16 12:43:02 -0600

    Use persistent data directory on Paradrop

--- server/default_settings.py
+++ server/default_settings.py
@@ -7,6 +7,8 @@ VIZAR_PORT = os.environ.get('VIZAR_PORT', 5000)
 # Host (and port) that should appear in URLs:
 VIZAR_EDGE_HOST = os.environ.get('VIZAR_EDGE_HOST', '127.0.0.1:5000')
 
-VIZAR_DATA_DIR='data'
+# Directory to store maps, headsets, and other data:
+VIZAR_DATA_DIR = os.environ.get('VIZAR_DATA_DIR', 'data')
+
 VIZAR_HEADSET_DIR='headsets'
 IMAGE_UPLOADS='/images/uploads/'
