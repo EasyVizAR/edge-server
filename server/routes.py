e2c9fb669241e69b7a714df17e8f542adb6eb6bd
Lance Hartung <hartung@cs.wisc.edu>
2021-12-06 14:38:13 -0600

    Add missing handler for / (index.html)

--- server/routes.py
+++ server/routes.py
@@ -1,11 +1,17 @@
 import os
 from http import HTTPStatus
-from quart import Blueprint, make_response, jsonify
+from quart import Blueprint, current_app, make_response, jsonify
 
 routes = Blueprint('routes', __name__)
 
-@routes.route('/icon_urls', methods=['GET'])
+
+@routes.route('/', methods=['GET'])
 async def index():
+    return await current_app.send_static_file('index.html')
+
+
+@routes.route('/icon_urls', methods=['GET'])
+async def list_icons():
     files = os.listdir('server/frontend/build/icons')
     totalFiles = []
     for file in files:
