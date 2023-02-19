5c8f78acd845724dcc2fce62e6e6cdaa6a26eae9
Rudy Banerjee <rudyb2001@gmail.com>
2023-02-19 10:04:10 -0600

    Moved testing code into it's own file
    Moved testing code from floor.py to a new gitignored file named test.py, as to not have test code on the main branch

--- server/mapping/floor.py
+++ server/mapping/floor.py
@@ -1,4 +1,4 @@
-# Za Wardo
+# Za Wardo (file initially named world.py)
 
 # Global set of data structures
 # Can have different threads of execution that can access all data threadsafe
@@ -13,7 +13,6 @@ import math
 import parse_data
 from grid import Grid
 import heapq
-import matplotlib.pyplot as plt
 
 class Floor():
 	def __init__(self, boxes_per_meter=5):
@@ -129,127 +128,3 @@ class Floor():
 		total_path.reverse()
 		return total_path
 
-##################
-# Test functions #
-##################
-
-def test_boundary_aware_bloom():
-	floor = Floor()
-	floor.update_user_locations_from_list([(0, 0)])
-	points = floor.boundary_aware_bloom()
-
-	locations = floor.user_locations
-	plt.plot([box[0] for box in locations], [box[1] for box in locations],
-			 marker="s", color="blue", markersize=2, linestyle = '')
-	
-	plt.plot([p[0] for p in points], [p[1] for p in points], "r.")
-
-	plt.show()
-
-def test_map_explore_area(user_locations_filename, destination_plot_filename):
-	plt.rcParams['figure.dpi'] = 500
-	plt.rcParams['savefig.dpi'] = 500
-
-	floor = Floor()
-	print("Updating walls...")
-	floor.update_walls_from_svg("image.svg")
-
-	print("Updating user locations...")
-	#floor.update_user_loations_from_csv("samples\\direct-route.csv")
-	floor.update_user_loations_from_csv(user_locations_filename)
-	#print([p for p in floor.user_locations])
-	
-	print("Naive bloom...")
-	floor.naive_bloom()
-
-	print("Calculating path...")
-	path = floor.calculatePath((0, -2), (20, 12))
-	#print(path)
-
-	weights = floor.explored_area
-	plt.plot([box[0] for box in weights], [box[1] for box in weights],
-			 marker="s", color="green", markersize=2, linestyle = '')
-
-	walls = floor.walls
-	plt.plot([box[0] for box in walls], [box[1] for box in walls],
-			 marker="s", color="black", markersize=2, linestyle = '')
-	
-	locations = floor.user_locations
-	plt.plot([box[0] for box in locations], [box[1] for box in locations],
-			 marker="s", color="blue", markersize=2, linestyle = '')
-
-	plt.plot([p[0] for p in path], [p[1] for p in path], "r-")
-
-	#plt.show()
-	#plt.savefig("images\\weighted_search_rh_search.png")
-	plt.savefig(destination_plot_filename)
-
-def generate_images():
-	test_map_explore_area("samples\\direct-route.csv", "images\\weighted_search_direct_route.png")
-	test_map_explore_area("samples\\rh-search.csv", "images\\weighted_search_rh_search.png")
-	test_map_explore_area("samples\\lh-search.csv", "images\\weighted_search_lh_search.png")
-
-def test_easy_explore_area():
-	floor = Floor(1)
-
-	print("Updating walls...")
-	boundary = []
-	floor.update_walls_from_list(boundary)
-
-	print("Updating user locations...")
-	locations = [(0, 0)]
-	floor.update_user_locations_from_list(locations)
-
-	print("Calculate naive bloom...")
-	floor.naive_bloom()
-
-	print("Calculate path...")
-	path = floor.calculatePath((-2, 2), (2, -4))
-
-	weights = floor.explored_area
-	plt.plot([box[0] for box in weights], [box[1] for box in weights],
-			 marker="s", color="green", markersize=2, linestyle = '')
-
-	walls = floor.walls
-	plt.plot([box[0] for box in walls], [box[1] for box in walls],
-			 marker="s", color="black", markersize=2, linestyle = '')
-
-	locations = floor.user_locations
-	plt.plot([box[0] for box in locations], [box[1] for box in locations],
-			 marker="s", color="red", markersize=2, linestyle = '')
-	
-	plt.plot([p[0] for p in path], [p[1] for p in path], "b-")
-
-	plt.show()
-
-
-
-
-
-def test_walls_locations_path():
-	floor = Floor()
-	print("Updating walls...")
-	floor.update_walls_from_svg("image.svg")
-
-	print("Updating user locations...")
-	floor.update_user_loations_from_csv("samples\\direct-route.csv")
-	#print([p for p in floor.user_locations])
-	
-	print("Calculating path...")
-	path = floor.calculatePath((0, -2), (20, 12))
-	#print(path)
-
-	walls = floor.walls
-	plt.plot([box[0] + 0.5 for box in walls], [box[1] + 0.5 for box in walls],
-			 marker="s", color="green", markersize=2, linestyle = '')
-	
-	locations = floor.user_locations
-	plt.plot([box[0] + 0.5 for box in locations], [box[1] + 0.5 for box in locations],
-			 marker="s", color="red", markersize=2, linestyle = '')
-
-	plt.plot([p[0] for p in path], [p[1] for p in path], "r-")
-
-	plt.show()
-
-if __name__ == "__main__":
-	generate_images()
