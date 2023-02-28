876299be58c69097d56d05c44918dbcaba4d2705
Rudy Banerjee <rudyb2001@gmail.com>
2023-02-27 23:28:57 -0600

    Added a user location append function

--- server/mapping/parse_data.py
+++ server/mapping/parse_data.py
@@ -5,6 +5,7 @@
 
 from xml.dom import minidom
 import csv
+import os
 
 def get_paths_from_svg(filename):
 
@@ -34,3 +35,27 @@ def get_user_locations_csv_hololens(filepath):
 				continue
 			points.append((float(row[1]), float(row[3])))
 	return points
+
+
+def append_user_location_to_csv(filename, point):
+    # Get the directory of the script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    # Create the full file path
+    file_path = os.path.join(script_dir, filename)
+    
+    # Create the file if it doesn't exist
+    if not os.path.isfile(file_path):
+        with open(file_path, 'w', newline='') as csvfile:
+            pass
+    
+    # Open the CSV file in append mode
+    with open(file_path, 'a', newline='') as csvfile:
+        # Create a CSV writer object
+        csvwriter = csv.writer(csvfile)
+        
+        # Write the point to the CSV file
+        csvwriter.writerow(point)
+
+if __name__ == '__main__':
+	append_user_location_to_csv("_test_csv_append.csv", (1, 1))
