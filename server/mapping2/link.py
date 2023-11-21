a4c9aee9a7f900ba26e198a33afc78c5fdd10709
Lance Hartung <lhartung@wisc.edu>
2023-11-21 14:05:56 -0600

    Add missing file

---
+++ server/mapping2/link.py
@@ -0,0 +1,5 @@
+class LinkEndpoint:
+    def __init__(self, chunk_id, face_index, sample_point):
+        self.chunk_id = chunk_id
+        self.face_index = face_index
+        self.sample_point = sample_point
